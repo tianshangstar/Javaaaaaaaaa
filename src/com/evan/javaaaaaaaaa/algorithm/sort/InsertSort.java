f38de375ab16b7267c24027fe05e1be476aec1f1
evan01.zhang <evan01.zhang@vipshop.com>
2018-11-23 16:52:29 +0800

    重修算法之插入排序（一丢丢的优化来自《数据结构与算法分析》）

---
+++ src/com/evan/javaaaaaaaaa/algorithm/sort/InsertSort.java
@@ -0,0 +1,26 @@
+package com.evan.javaaaaaaaaa.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by evan01.zhang on 2018/11/23.
+ */
+public class InsertSort {
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // 选择排序过的下一个元素，反向遍历
+            for (int j = i; j > 0 && arr[j] > arr[j - 1]; j--) {
+                arr[j] = arr[j] ^ arr[j - 1];
+                arr[j - 1] = arr[j] ^ arr[j - 1];
+                arr[j] = arr[j] ^ arr[j - 1];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //待排序数组
+        int[] arr = {15, 24, 15, 9, 17, 44, 48, 5, 9, 3, 48, 10};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
