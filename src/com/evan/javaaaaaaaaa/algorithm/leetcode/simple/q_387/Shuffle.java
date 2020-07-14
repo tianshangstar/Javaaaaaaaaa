652a7e06352ce87d870d73d1daf0ac8dcea380ad
evan01.zhang <evan01.zhang@vipshop.com>
2020-07-14 14:33:46 +0800

    每日作业：leetCode q387

---
+++ src/com/evan/javaaaaaaaaa/algorithm/leetcode/simple/q_387/Shuffle.java
@@ -0,0 +1,16 @@
+package com.evan.javaaaaaaaaa.algorithm.leetcode.simple.q_387;
+
+import com.alibaba.fastjson.JSON;
+
+public class Shuffle {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        Solution solution = new Solution(arr);
+
+        for (int i = 0; i < 3; i++) {
+            System.err.println("------- " + i + " -------");
+            System.err.println(JSON.toJSONString(solution.shuffle()));
+            System.err.println(JSON.toJSONString(solution.reset()));
+        }
+    }
+}
---
+++ src/com/evan/javaaaaaaaaa/algorithm/leetcode/simple/q_387/Solution.java
@@ -0,0 +1,48 @@
+package com.evan.javaaaaaaaaa.algorithm.leetcode.simple.q_387;
+
+import java.util.Random;
+
+public class Solution {
+
+    private final int[] source;
+    private int[] target;
+
+    private Random random = new Random(47);
+
+
+    public Solution(int[] nums) {
+        this.source = nums;
+        this.target = source.clone();
+    }
+
+    /**
+     * Resets the array to its original configuration and return it.
+     */
+    public int[] reset() {
+        this.target = source.clone();
+        return target;
+    }
+
+    /**
+     * Returns a random shuffling of the array.
+     */
+    public int[] shuffle() {
+        long start = System.nanoTime();
+        target = source.clone();
+        for (int i = 0; i < target.length; i++) {
+            int anchor = random.nextInt(target.length - i) + i;
+            swap(i, anchor);
+        }
+        System.err.println("cost:" + (System.nanoTime() - start));
+        return target;
+    }
+
+    private void swap(int h, int t) {
+        if (h == t)
+            return;
+        target[h] ^= target[t];
+        target[t] = target[h] ^ target[t];
+        target[h] = target[h] ^ target[t];
+    }
+
+}
