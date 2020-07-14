2478820192d347b2f2f78f3b4cd0218484ee5068
evan01.zhang <evan01.zhang@vipshop.com>
2020-07-14 09:19:46 +0800

    add comments, add test case

--- src/com/evan/javaaaaaaaaa/algorithm/leetcode/simple/q_26/RemoveDuplicates.java
+++ src/com/evan/javaaaaaaaaa/algorithm/leetcode/simple/q_26/RemoveDuplicates.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSON;
 public class RemoveDuplicates {
 
     public static void main(String[] args) {
-        int[] arr = {1, 1, 2, 3, 3, 4, 5};
+        int[] arr = {1, 2, 3, 3, 4, 5};
         Solution solution = new Solution();
         int len = solution.removeDuplicates(arr);
         System.err.println("len = " + len);
--- src/com/evan/javaaaaaaaaa/algorithm/leetcode/simple/q_26/Solution.java
+++ src/com/evan/javaaaaaaaaa/algorithm/leetcode/simple/q_26/Solution.java
@@ -17,6 +17,7 @@ public class Solution {
         int h = 1;
         int t = 1;
         int v = nums[0];
+        // 如果初始化 h = t = 0的话，就不需要v了（因为这样v一直等于nums[h]）
         while (t < size) {
             if (v != nums[t]) {
                 v = nums[h] = nums[t];
